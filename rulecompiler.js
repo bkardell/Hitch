41667311bf01eb6e4a33114dbf6d439f1a4d382c
Brian <bkardell@gmail.com>
2011-12-14 08:57:45 -0500

    Rule compiler now also works with http: based urls for @-plugins-require
    And another temporary patch for fixing something that is duplicate work
    because of the current patterns.... ick.

--- rulecompiler.js
+++ rulecompiler.js
@@ -1,21 +1,61 @@
 var fs = require('fs'),
+    url = require('url'),
+    http = require('http'),
     cssPluginsCompiler = require('./lib/cssPluginsCompiler'), 
     src,
     compiledRulesBuff = [], 
     compiledCSSRulesFile, 
-    compiledJSRulesFile;
+    compiledJSRulesFile,
+    inPath = process.argv[2],
+    reading = [],
+    outPath = (process.argv.length>3) ? process.argv[3]: 'dist', 
+    checker;
     
+checker = function(cb){
+	for(var i=0;i<reading.length;i++){
+		if(!reading[i]){
+			setTimeout(function(){ checker(cb); }, 100);			
+			return false;
+		}
+	};
+	cb();
+};
+
 //setup some global shit to make this work / pretend we are a browser 'just enough'   
 $ = {
 	getScript: function(x){
-		var cssRealPath = fs.realpathSync(process.argv[2]);
-		cssRealPath = cssRealPath.substring(0,cssRealPath.lastIndexOf('/') + 1);
-		eval(fs.readFileSync(cssRealPath + x, 'UTF-8'));	
+		var cssRealPath;
+		if(x.indexOf('http:') === 0){
+			cssRealPath = url.parse(x);
+			reading.push(false);
+			http.get(
+				{
+					host: cssRealPath.host, 
+					port: 80, 
+					path: cssRealPath.pathname
+				},
+				function(res){
+					var buff = [];
+					res.on('data', function(chunk){
+						buff.push(chunk);
+					});
+					res.on('end', function(){
+						eval(buff.join(''));
+						reading.pop();
+					});
+				
+				}
+			);
+		}else{
+			cssRealPath = fs.realpathSync(process.argv[2]);
+			cssRealPath = cssRealPath.substring(0,cssRealPath.lastIndexOf('/') + 1);
+			eval(fs.readFileSync(cssRealPath + x, 'UTF-8'));	
+		};
 	}, 
 	when: function(){
 		return {
-			then: function(t){
-				t();
+			then: function(next){
+				checker(next);
 			}
 		}
 	}
@@ -40,8 +80,8 @@ cssPluginsCompiler(src, function(o){
 	}
 	compiledCSSRulesFile = process.argv[2].replace('.css','-compiled.css');
 	compiledJSRulesFile = process.argv[2].replace('.css', '-compiled.js');
-	compiledCSSRulesFile = "dist" +  compiledCSSRulesFile.substring(compiledCSSRulesFile.lastIndexOf("/"));
-	compiledJSRulesFile = "dist" +  compiledJSRulesFile.substring(compiledJSRulesFile.lastIndexOf("/"));
+	compiledCSSRulesFile = outPath +  compiledCSSRulesFile.substring(compiledCSSRulesFile.lastIndexOf("/"));
+	compiledJSRulesFile = outPath +  compiledJSRulesFile.substring(compiledJSRulesFile.lastIndexOf("/"));
 	
 	console.log('writing compiled css file...' + compiledCSSRulesFile);
 	fs.writeFileSync(
